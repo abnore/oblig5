7b6470297a04456011472f2396ad40d38794eedb
Andreas Nore <andreas.nore@gmail.com>
2023-04-02 17:12:17 +0200

    new file:   Oblig5Del2/Hoved.java 	new file:   Oblig5Del2/Subsekvens.java 	new file:   Oblig5Del2/SubsekvensRegister.java

---
+++ Oblig5Del2/Subsekvens.java
@@ -0,0 +1,26 @@
+/*
+ * Oppgave 1 
+ *  ↳ Tar var på en subsekvens (String) og et antall (et heltall)
+ *  ↳ Lag en toString metode som skriver ut e.g (ABC,4)
+ */
+
+public class Subsekvens {
+    public final String subsekvens;
+    private int antall;
+
+    public Subsekvens(String subsekvens, int antall){
+        this.subsekvens = subsekvens;
+        this.antall = antall;
+    }
+    public int hentAntall(){
+        return antall;
+    }
+    public void endreAntall(int nyttTall){
+        antall = nyttTall;
+    }
+
+    @Override
+    public String toString(){
+        return "("+subsekvens+","+antall+")";
+    }
+}
